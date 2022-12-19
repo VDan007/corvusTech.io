f1ca39fcb49fd0745bf656bbba3d39c34d82e32a
Dániel Varjaskéri <varjaskerid@gmail.com>
2022-12-19 16:19:56 +0100

    nice

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ myImage.addEventListener('load',()=>{
     canvas.height = 538;
     
     ctx.drawImage(myImage,0,0,canvas.width,canvas.height);
+    const pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+    console.log(pixels);
 
-    let particleArray = [];
+    let particlesArray = [];
     const numberOfParticles = 5000;
+
+    let mappedImage = [];
+    for (let y = 0; y < canvas.height; y++){
+        let row = [];
+        for (let x = 0; x < canvas.width; x++){
+            const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+            const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+            const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+            const brightness = calculateRelativeBrightness(red,green,blue);
+            const cell = [
+                cellBrightness = brightness,
+            ];
+            row.push(cell);
+        }
+        mappedImage.push(row);
+    }
+    function calculateRelativeBrightness(red,green,blue){
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green) * 0.587 +
+            (blue * blue) * 0.114
+        )/100;
+    }
 
     class Particle{
         constructor(){
@@ -28,6 +53,29 @@ myImage.addEventListener('load',()=>{
                 this.x = Math.random() * canvas.width;
             }
         }
+        draw(){
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(this.x,this.y,this.size,0,Math.PI *2);
+            ctx.fill();
+        }
     }
+    function init(){
+        for(let i = 0; i<numberOfParticles; i++){
+            particlesArray.push(new Particle);
+        }
+    }
+    init();
+    function animate(){
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = "rgb(0,0,0)";
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        for(let i = 0; i < particlesArray.length; i++){
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
 
